d3912a234c6c4938637f037413d586b21ddf5098
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2024-11-01 17:23:19 -0400

    hello

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,8 @@ import { Logo } from '@/components/Assets/Logo';
 import { useRouter, usePathname } from 'next/navigation';
 import { useLocale, useTranslations } from 'next-intl';
 import { Link } from '@/i18n/routing';
+import { HiOutlineMenu, HiOutlineX } from 'react-icons/hi';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function Header() {
   const t = useTranslations('header');
@@ -11,6 +13,7 @@ export default function Header() {
   const pathname = usePathname();
   const currentLocale = useLocale();
   const [locale, setLocale] = useState(currentLocale);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleLocaleChange = (newLocale: string) => {
     const pathSegments = pathname.split('/');
@@ -21,17 +24,19 @@ export default function Header() {
     router.push(newPathname);
   };
 
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
-    <header className="flex justify-between items-center p-4 px-10 w-full">
-      <Link href={`/${locale}`}>
-        <Logo className="h-20 w-auto" invert={true} />
+    <header className="flex justify-between items-center p-4 px-6 sm:px-10 w-full">
+      <Link href={`/`}>
+        <Logo className="h-14 w-auto sm:h-20" invert={true} />
       </Link>
 
-      <nav className="flex items-center space-x-6">
-        <div className="flex items-center space-x-2">
-          <button
+      <nav className="hidden sm:flex items-center space-x-6">
+      <div className="flex items-center space-x-2 ">
+        <button
             onClick={() => handleLocaleChange('en')}
-            className={`text-white text-sm transition-opacity duration-300 font-thin ${
+            className={`text-white text-sm ${
               locale === 'en' ? 'opacity-100' : 'opacity-50'
             }`}
           >
@@ -40,7 +45,7 @@ export default function Header() {
           <span className="text-white font-thin">|</span>
           <button
             onClick={() => handleLocaleChange('es')}
-            className={`text-white text-sm transition-opacity duration-300 font-thin ${
+            className={`text-white text-sm ${
               locale === 'es' ? 'opacity-100' : 'opacity-50'
             }`}
           >
@@ -49,23 +54,77 @@ export default function Header() {
         </div>
         <Link
           href={`/`}
-          className="text-white hover:text-gray-400 transition-opacity duration-300 font-extralight hover:opacity-75"
+          className="text-white text-sm hover:text-gray-400 transition-opacity duration-300 font-extralight"
         >
           {t('home')}
         </Link>
         <Link
           href={`/about`}
-          className="text-white hover:text-gray-400 transition-opacity duration-300 font-extralight hover:opacity-75"
+          className="text-white text-sm hover:text-gray-400 transition-opacity duration-300 font-extralight"
         >
           {t('about')}
         </Link>
         <Link
           href={`/contact`}
-          className="text-white hover:text-gray-400 transition-opacity duration-300 font-extralight hover:opacity-75"
+          className="text-white text-sm hover:text-gray-400 transition-opacity duration-300 font-extralight"
         >
           {t('contact')}
         </Link>
       </nav>
+
+      <div className="flex items-center space-x-2 sm:hidden">
+        <button
+          onClick={() => handleLocaleChange('en')}
+          className={`text-white text-sm ${
+            locale === 'en' ? 'opacity-100' : 'opacity-50'
+          }`}
+        >
+          EN
+        </button>
+        <span className="text-white font-thin">|</span>
+        <button
+          onClick={() => handleLocaleChange('es')}
+          className={`text-white text-sm ${
+            locale === 'es' ? 'opacity-100' : 'opacity-50'
+          }`}
+        >
+          ES
+        </button>
+
+        <button onClick={toggleMenu} className="text-white text-2xl">
+          <HiOutlineMenu />
+        </button>
+      </div>
+
+      <AnimatePresence>
+        {menuOpen && (
+          <motion.div
+            className="fixed inset-0 bg-black bg-opacity-75 flex flex-col items-end p-8 z-50"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <button
+              onClick={toggleMenu}
+              className="text-white text-3xl mb-8"
+            >
+              <HiOutlineX />
+            </button>
+            <nav className="flex flex-col text-right font-semibold  text-white text-3xl space-y-8">
+              <Link href={`/`} onClick={toggleMenu}>
+                {t('home')}
+              </Link>
+              <Link href={`/about`} onClick={toggleMenu}>
+                {t('about')}
+              </Link>
+              <Link href={`/contact`} onClick={toggleMenu}>
+                {t('contact')}
+              </Link>
+            </nav>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 }
--- src/components/sections/landing/index.tsx
+++ src/components/sections/landing/index.tsx
@@ -2,11 +2,7 @@
 import { useTranslations } from 'next-intl';
 import { Container } from '@/components/Container';
 import { FadeIn } from '@/components/effects/FadeIn';
-import {
-  CodeBracketIcon,
-  LifebuoyIcon,
-  GlobeAltIcon,
-} from '@heroicons/react/24/solid';
+import { CodeBracketIcon, LifebuoyIcon, GlobeAltIcon } from '@heroicons/react/24/solid';
 import { motion } from 'framer-motion';
 
 export default function LandingSection() {
@@ -32,15 +28,15 @@ export default function LandingSection() {
 
   return (
     <motion.div
-      className="flex flex-col justify-center items-center pt-24 px-24"
+      className="flex flex-col justify-center items-center pt-12 sm:pt-24 px-4 sm:px-24"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1, ease: 'easeIn' }}
     >
       <Container className="flex justify-center items-center">
-        <FadeIn className="text-center max-w-5xl">
+        <FadeIn className="text-center max-w-2xl sm:max-w-5xl">
           <motion.h1
-            className="font-display text-5xl font-medium tracking-tight text-white [text-wrap:balance] sm:text-5xl"
+            className="font-display text-3xl sm:text-5xl font-medium tracking-tight text-white [text-wrap:balance]"
             initial={{ y: -20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, ease: 'easeIn' }}
@@ -48,7 +44,7 @@ export default function LandingSection() {
             {t('pageIntro.title')}
           </motion.h1>
           <motion.p
-            className="mt-6 text-xl text-gray-300"
+            className="mt-4 sm:mt-6 text-base sm:text-xl text-gray-300"
             initial={{ y: 20, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.2, ease: 'easeIn' }}
@@ -58,7 +54,7 @@ export default function LandingSection() {
         </FadeIn>
       </Container>
       <motion.div
-        className="mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 lg:max-w-none lg:grid-cols-3 lg:gap-8"
+        className="mt-10 sm:mt-16 grid max-w-xl grid-cols-1 gap-4 sm:max-w-2xl sm:gap-6 lg:max-w-none lg:grid-cols-3 lg:gap-8"
         initial="hidden"
         animate="visible"
         variants={{
@@ -70,7 +66,7 @@ export default function LandingSection() {
         {cards.map((card) => (
           <motion.div
             key={card.name}
-            className="flex gap-x-4 rounded-xl bg-white/5 p-6 ring-1 ring-inset ring-white/10"
+            className="flex gap-x-4 rounded-lg bg-white/5 p-4 sm:p-6 ring-1 ring-inset ring-white/10"
             variants={{
               hidden: { opacity: 0, y: 20 },
               visible: { opacity: 1, y: 0 },
@@ -79,11 +75,11 @@ export default function LandingSection() {
           >
             <card.icon
               aria-hidden="true"
-              className="h-7 w-5 flex-none text-indigo-400"
+              className="h-6 w-6 sm:h-7 sm:w-7 flex-none text-indigo-400"
             />
-            <div className="text-base">
+            <div className="text-sm sm:text-base">
               <h3 className="font-semibold text-white">{card.name}</h3>
-              <p className="mt-2 text-gray-300">{card.description}</p>
+              <p className="mt-1 sm:mt-2 text-gray-300">{card.description}</p>
             </div>
           </motion.div>
         ))}
